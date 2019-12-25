ece88758bbec8f0bc3e3b2eea0e4c019b0be5646
Szczepan Ćwikliński <sec@thinkcode.pl>
2019-12-25 17:55:28 +0100

    Day 24 Part 2

--- aoc2019.tests/Day24UnitTests.cs
+++ aoc2019.tests/Day24UnitTests.cs
@@ -5,10 +5,19 @@ public class Day24UnitTests
 {
     [Theory]
     [InlineData(14539258)]
-    public void Day24_Part1(long part1)
+    public void Day24_Part1(long ans)
     {
         var d = new Day24();
 
-        Assert.Equal(part1.ToString(), d.Part1());
+        Assert.Equal(ans.ToString(), d.Part1());
+    }
+
+    [Theory]
+    [InlineData(1977)]
+    public void Day24_Part2(long ans)
+    {
+        var d = new Day24();
+
+        Assert.Equal(ans.ToString(), d.Part2());
     }
 }
--- aoc2019/Code/Day24.cs
+++ aoc2019/Code/Day24.cs
@@ -1,57 +1,125 @@
 ﻿using aoc2019.Misc;
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace aoc2019.Code
 {
     public class Day24 : BaseDay
     {
+        static readonly Dictionary<int, Eris> Planet = new Dictionary<int, Eris>();
+
         public Day24() : base(24)
         {
         }
 
-        class Eris
+        public class Eris
         {
             const int N = 5;
 
-            BitArray _eris, _temp;
+            private BitArray _eris, _temp;
+            private readonly bool _multiLevel;
+            private readonly int _level;
 
-            public Eris(string data)
+            public Eris(string data, bool isMultiLevel, int level)
             {
+                _level = level;
+                _multiLevel = isMultiLevel;
+
                 _eris = new BitArray(N * N);
                 _temp = new BitArray(N * N);
 
-                int i = 0;
-                foreach (var c in data)
+                if (!string.IsNullOrWhiteSpace(data))
                 {
-                    if (c != '.' && c != '#')
+                    int i = 0;
+                    foreach (var c in data)
                     {
-                        continue;
+                        if (c != '.' && c != '#')
+                        {
+                            continue;
+                        }
+                        _eris.Set(i++, c == '#');
                     }
-                    _eris.Set(i++, c == '#');
                 }
             }
 
-            int Count(int x, int y, bool what)
+            private int Count(int x, int y, bool what)
             {
                 var c = 0;
                 foreach (var m in Ext.GetNextMove(x, y))
                 {
                     if (m.x < 0 || m.x >= N || m.y < 0 || m.y >= N)
                     {
-                        continue;
+                        var _prev = Planet.GetValueOrDefault(_level - 1);
+
+                        if (_multiLevel && _prev != null)
+                        {
+                            // up
+                            if (m.x < 0 && _prev.Get(1, 2))
+                            {
+                                c++;
+                            }
+                            // right
+                            else if (m.x >= N && _prev.Get(3, 2))
+                            {
+                                c++;
+                            }
+                            // bottom
+                            else if (m.y < 0 && _prev.Get(2, 1))
+                            {
+                                c++;
+                            }
+                            // left
+                            else if (m.y >= N && _prev.Get(2, 3))
+                            {
+                                c++;
+                            }
+                        }
                     }
-                    if (Get(m.x, m.y) == what)
+                    else if (Get(m.x, m.y) == what)
                     {
                         c++;
                     }
                 }
+
+                if (_multiLevel)
+                {
+                    c += CountNextLevel(x, y);
+                }
+
                 return c;
             }
 
-            bool Get(int x, int y) => _eris.Get(y * N + x);
+            private int CountNextLevel(int x, int y)
+            {
+                var count = 0;
+
+                var _next = Planet.GetValueOrDefault(_level + 1);
+                if (_next == null)
+                {
+                    return count;
+                }
+
+                count += (x, y) switch
+                {
+                    // get 5 top rows from next level
+                    (2, 1) => CountH(0, _next),
+
+                    // get 5 right rows from next level
+                    (3, 2) => CountV(N - 1, _next),
+
+                    // get 5 bottom rows from next level
+                    (2, 3) => CountH(N - 1, _next),
+
+                    // get 5 left rows from next level
+                    (1, 2) => CountV(0, _next),
+                    _ => 0
+                };
+
+                return count;
+            }
+
+            public bool Get(int x, int y) => _eris.Get(y * N + x);
 
             void Set(int x, int y, bool what) => _temp.Set(y * N + x, what);
 
@@ -61,20 +129,32 @@ namespace aoc2019.Code
                 {
                     for (int x = 0; x < N; x++)
                     {
-                        if (Get(x, y) && Count(x, y, true) != 1)
+                        if (_multiLevel && x == 2 && y == 2)
+                        {
+                            continue;
+                        }
+
+                        var count = Count(x, y, true);
+                        var current = Get(x, y);
+
+                        if (current && count != 1)
                         {
                             Set(x, y, false);
                         }
-                        else if (Get(x, y) == false && (Count(x, y, true) == 1 || Count(x, y, true) == 2))
+                        else if (current == false && (count == 1 || count == 2))
                         {
                             Set(x, y, true);
                         }
                         else
                         {
-                            Set(x, y, Get(x, y));
+                            Set(x, y, current);
                         }
                     }
                 }
+            }
+
+            public void Swap()
+            {
                 var tmp = _eris;
                 _eris = _temp;
                 _temp = tmp;
@@ -87,11 +167,39 @@ namespace aoc2019.Code
 
                 return a[0];
             }
+
+            public int CountBugs() => _eris.OfType<bool>().Where(x => x).Count();
+
+            private static int CountH(int y, Eris eris)
+            {
+                var c = 0;
+                for (int i = 0; i < N; i++)
+                {
+                    if (eris.Get(i, y))
+                    {
+                        c++;
+                    }
+                }
+                return c;
+            }
+
+            private static int CountV(int x, Eris eris)
+            {
+                var c = 0;
+                for (int i = 0; i < N; i++)
+                {
+                    if (eris.Get(x, i))
+                    {
+                        c++;
+                    }
+                }
+                return c;
+            }
         }
 
         public override string Part1()
         {
-            var eris = new Eris(ReadAllText());
+            var eris = new Eris(ReadAllText(), false, 0);
             var set = new DefaultDict<int, int>();
 
             while (true)
@@ -104,12 +212,30 @@ namespace aoc2019.Code
                     return r.ToString();
                 }
                 eris.Tick();
+                eris.Swap();
             }
         }
 
         public override string Part2()
         {
-            return string.Empty;
+            foreach (var i in Enumerable.Range(-100, 201))
+            {
+                Planet[i] = new Eris(i == 0 ? ReadAllText() : string.Empty, true, i);
+            }
+
+            foreach (var n in Enumerable.Range(0, 200))
+            {
+                foreach (var e in Planet.Values)
+                {
+                    e.Tick();
+                }
+                foreach (var e in Planet.Values)
+                {
+                    e.Swap();
+                }
+            }
+
+            return Planet.Values.Sum(x => x.CountBugs()).ToString();
         }
     }
 }
